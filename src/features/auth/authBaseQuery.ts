d2fd2271f2430e2a4d5bb8a2038d8f8ffa77a46c
vishnupreman <vishnupreman1011@gmail.com>
2025-08-15 14:38:28 +0530

    Testing...

--- src/features/auth/authBaseQuery.ts
+++ src/features/auth/authBaseQuery.ts
@@ -4,6 +4,7 @@ import type { RootState } from "../../app/store";
 import { setCredentials, logout } from "./authSlice";
 
 const baseUrl = 'https://time-tracker-app-backend-1.onrender.com/api'
+// const baseUrl = 'http://localhost:5000/api'
 
 export const baseQueryWithReauth: BaseQueryFn<string | FetchArgs, unknown, FetchBaseQueryError> =
   async (args, api, extraOptions) => {
@@ -21,7 +22,7 @@ export const baseQueryWithReauth: BaseQueryFn<string | FetchArgs, unknown, Fetch
     let result = await baseQuery(args, api, extraOptions);
 
     if (result.error && result.error.status === 403) {
-      const refreshResult = await baseQuery({url: "/auth/refresh-token", method: 'POST', credentials: "include"}, api, extraOptions);
+      const refreshResult = await baseQuery("/auth/refresh-token", api, extraOptions);
       if (refreshResult.data) {
         const newAccessToken = (refreshResult.data as any).accessToken;
         api.dispatch(setCredentials({ accessToken: newAccessToken, user: (api.getState() as RootState).auth.user! }));
